be2207c7f67bc60121ee32228420a574b2754c7b
ngoctinn <3121410041@sv.sgu.edu.vn>
2024-08-09 22:00:09 +0700

    chuong1-bai5

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ console.log("Hello World!");
 // camelCase    -   firstName
 // i love my school  -   iLoveMySchool
 // Không đặt biến tên tiếng việt hoặc có dấu hoặc ký tự đặc biệt
+//Declare a variable
+// const and let
+const number = 10;
+console.log(number);
+// const number = 20; (Error)(không thể gán lại giá trị cho const)
+let number1 = 20;
+number1 = 30;
+console.log(number1);
+
+let string = "Welcome to JS COURSE";
+console.log(string);
