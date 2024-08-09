de2ea9ae677e4ebceae49fb59fd9389f2fab64d1
ngoctinn <3121410041@sv.sgu.edu.vn>
2024-08-09 21:53:40 +0700

    chuong1-bai4

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Hello World!");
+// Biến - Variable
+// camelCase    -   firstName
+// i love my school  -   iLoveMySchool
+// Không đặt biến tên tiếng việt hoặc có dấu hoặc ký tự đặc biệt
